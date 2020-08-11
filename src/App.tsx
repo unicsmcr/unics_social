945eb122a805a5f9cacf320a7da5d718a1726555
HishamYahya <38282695+HishamYahya@users.noreply.github.com>
2020-08-11 18:01:59 +0300

    feat: implemented routing and initialised page components (#1)
    * feat: implemented routing and initialised page components
    
    * refactor: added lines at the end of tsx files and deleted App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import HomePage from './components/routes/HomePage';
+import EventsPage from './components/routes/EventsPage';
+import ChatPage from './components/routes/ChatPage';
+import NetworkingPage from './components/routes/NetworkingPage';
+import Header from './components/Header';
+import FriendsPage from './components/routes/FriendsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/events' component={EventsPage} />
+          <Route path='/chat' component={ChatPage} />
+          <Route path='/friends' component={FriendsPage} />
+          <Route path='/networking' component={NetworkingPage} />
+
+          <Redirect to='/' />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/routes/FriendsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FriendsPage = () => {
+    return <div>FriendsPage</div>
+}
+
+export default FriendsPage;
