e66a9bd0e69ffb52ba7e8e335452a6f8598654b6
Amish Shah <amishshah.2k@gmail.com>
2020-08-29 11:31:57 +0100

    fix: change password type

--- src/components/routes/RegistrationPage.tsx
+++ src/components/routes/RegistrationPage.tsx
@@ -117,7 +117,7 @@ export default function RegistrationPage() {
 					<TextField variant="outlined" { ...buildProps('forename', 'Forename') } />
 					<TextField variant="outlined" { ...buildProps('surname', 'Surname') } />
 					<TextField variant="outlined" InputProps={{ type: 'email' }} { ...buildProps('email', 'Email') }/>
-					<TextField variant="outlined" InputProps={{ type: 'email' }} { ...buildProps('password', 'Password') } />
+					<TextField variant="outlined" InputProps={{ type: 'password' }} { ...buildProps('password', 'Password') } />
 					<Button variant="contained" color="primary" type="submit">
 						Register
 					</Button>
