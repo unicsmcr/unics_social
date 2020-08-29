98e706eeab7c8136ed297b54a8bb0700daae56cf
Amish Shah <amishshah.2k@gmail.com>
2020-08-29 13:56:41 +0100

    refactor: restructure store

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import store from './redux/store';
 
 ReactDOM.render(
 	<React.StrictMode>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import AuthReducer from '../features/AuthSlice';
-import EventsListReducer from '../features/EventsListSlice';
-import ChannelListReducer from '../features/ChannelsSlice';
+import AuthReducer from './slices/AuthSlice';
+import EventsListReducer from './slices/EventsListSlice';
+import ChannelListReducer from './slices/ChannelsSlice';
+
 export default configureStore({
 	reducer: {
 		auth: AuthReducer,
