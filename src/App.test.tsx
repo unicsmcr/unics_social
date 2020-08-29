89ff420b01f585fa9147e863f2eef33dbec7284d
Amish Shah <amishshah.2k@gmail.com>
2020-08-29 19:13:05 +0100

    test: update test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders copyright', () => {
 	const { getByText } = render(<App />);
 	const copyright = getByText(/Copyright/i);
 	expect(copyright).toBeInTheDocument();
