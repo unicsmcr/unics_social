c18ebd991fd024ae3312b96fc39654452eaf9552
Amish Shah <amishshah.2k@gmail.com>
2020-08-31 20:09:24 +0100

    feat: add users list

--- src/components/routes/Chat.tsx
+++ src/components/routes/Chat.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Page from '../Page';
 import List from '@material-ui/core/List';
-import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import { Paper, AppBar, Tabs, Tab } from '@material-ui/core';
+import { Paper, AppBar, Tabs, Tab, Avatar, ListItemAvatar } from '@material-ui/core';
+
+const dummyUsers = [
+	{ name: 'Barack Obama', src: 'https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg' },
+	{ name: 'Azealia Banks', src: 'https://lh3.googleusercontent.com/proxy/4gdPHEtUwkjRLpp08zdJbTPE2Bh8XOwJY_rPpoSl43aqrvMHFdoaIBhq3BYpC4Na6mjbqFB1Zop_1rL6RCi2tcoI7FtLK4vRdb0MKu1Ia9FSPdWI3oohcmoUwm8' },
+	{ name: 'Mario', src: 'https://sickr.files.wordpress.com/2017/07/mario.jpg' },
+	{ name: 'Luigi', src: 'https://www.mariowiki.com/images/thumb/5/53/Luigi_Mario_Party.png/158px-Luigi_Mario_Party.png' }
+];
 
 const useStyles = makeStyles(theme => ({
 	mainContent: {
@@ -19,65 +22,58 @@ const useStyles = makeStyles(theme => ({
 		textAlign: 'center'
 	},
 	chatsRoot: {
-		height: '80vh',
 		display: 'flex'
 	},
-	channelsList: {
+	channelsPanel: {
 		width: theme.spacing(40)
+	},
+	channelsList: {
+		minHeight: '80vh',
+		maxHeight: '80vh',
+		overflow: 'auto'
 	},
 	flexGrow: {
 		flexGrow: 1
 	}
 }));
 
-function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
-	return <ListItem button component="a" {...props} />;
+function UserListItem({ name, src }: {name: string; src: string}) {
+	return <ListItem button>
+		<ListItemAvatar>
+			<Avatar alt={name} src={src}/>
+		</ListItemAvatar>
+		<ListItemText primary={name} />
+	</ListItem>;
 }
 
-export default function ChatPage() {
+export function ChannelsPanel() {
 	const classes = useStyles();
 
 	const [chatPanelValue, setChatPanelValue] = React.useState(0);
 
-	const chatsPanel = <Paper elevation={2}>
-		<div className={classes.channelsList}>
+	return <Paper elevation={2}>
+		<div className={classes.channelsPanel}>
 			<AppBar position="static" color="primary">
 				<Tabs variant="fullWidth" value={chatPanelValue} onChange={(_, v) => setChatPanelValue(v)} indicatorColor="secondary" textColor="inherit">
 					<Tab label="Users" />
 					<Tab label="Events" />
 				</Tabs>
 			</AppBar>
-			<List component="nav" aria-label="main mailbox folders">
-				<ListItem button>
-					<ListItemIcon>
-						<InboxIcon />
-					</ListItemIcon>
-					<ListItemText primary="Inbox" />
-				</ListItem>
-				<ListItem button>
-					<ListItemIcon>
-						<DraftsIcon />
-					</ListItemIcon>
-					<ListItemText primary="Drafts" />
-				</ListItem>
-			</List>
-			<Divider />
-			<List component="nav" aria-label="secondary mailbox folders">
-				<ListItem button>
-					<ListItemText primary="Trash" />
-				</ListItem>
-				<ListItemLink href="#simple-list">
-					<ListItemText primary="Spam" />
-				</ListItemLink>
+			<List component="nav" aria-label="main mailbox folders" className={classes.channelsList} >
+				{dummyUsers.map(user => <UserListItem {...user} />)}
 			</List>
 		</div>
 	</Paper>;
+}
+
+export default function ChatPage() {
+	const classes = useStyles();
 
 	return (
 		<Page>
 			<Container maxWidth="xl" component="main" className={classes.mainContent}>
 				<Paper elevation={3} className={classes.chatsRoot}>
-					{ chatsPanel }
+					<ChannelsPanel />
 					<div className={classes.flexGrow}>
 						<Typography>hi</Typography>
 					</div>
