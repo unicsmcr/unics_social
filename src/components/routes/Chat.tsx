49b7f85a2b52f2679bf96752661a3d2ac076ca22
Amish Shah <amishshah.2k@gmail.com>
2020-08-31 18:25:23 +0100

    feat: start work on layout

--- src/components/routes/Chat.tsx
+++ src/components/routes/Chat.tsx
@@ -2,35 +2,88 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Page from '../Page';
+import List from '@material-ui/core/List';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import { Paper, AppBar, Tabs, Tab } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-	heroContent: {
-		padding: theme.spacing(14, 2, 4, 2)
-	},
 	mainContent: {
-		padding: theme.spacing(4, 2, 14, 2),
+		padding: theme.spacing(6, 2, 14, 2),
 		textAlign: 'center'
+	},
+	chatsRoot: {
+		height: '80vh',
+		display: 'flex'
+	},
+	channelsList: {
+		width: theme.spacing(40)
+	},
+	flexGrow: {
+		flexGrow: 1
 	}
 }));
 
+function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
+	return <ListItem button component="a" {...props} />;
+}
+
 export default function ChatPage() {
 	const classes = useStyles();
+	const theme = useTheme();
+
+	const [chatPanelValue, setChatPanelValue] = React.useState(0);
+
+	const chatsPanel = <Paper elevation={2}>
+		<div className={classes.channelsList}>
+			<AppBar position="static" color="primary">
+				<Tabs variant="fullWidth" value={chatPanelValue} onChange={(_, v) => setChatPanelValue(v)} indicatorColor="secondary" textColor="inherit">
+					<Tab label="Users" />
+					<Tab label="Events" />
+				</Tabs>
+			</AppBar>
+			<List component="nav" aria-label="main mailbox folders">
+				<ListItem button>
+					<ListItemIcon>
+						<InboxIcon />
+					</ListItemIcon>
+					<ListItemText primary="Inbox" />
+				</ListItem>
+				<ListItem button>
+					<ListItemIcon>
+						<DraftsIcon />
+					</ListItemIcon>
+					<ListItemText primary="Drafts" />
+				</ListItem>
+			</List>
+			<Divider />
+			<List component="nav" aria-label="secondary mailbox folders">
+				<ListItem button>
+					<ListItemText primary="Trash" />
+				</ListItem>
+				<ListItemLink href="#simple-list">
+					<ListItemText primary="Spam" />
+				</ListItemLink>
+			</List>
+		</div>
+	</Paper>;
 
 	return (
 		<Page>
-			{/* Hero unit */}
-			<Container maxWidth="sm" component="header" className={classes.heroContent}>
-				<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-					Account Settings
-				</Typography>
-			</Container>
-			{/* End hero unit */}
-			<Container maxWidth="sm" component="main" className={classes.mainContent}>
-				<Typography component="p" variant="body1">
-					Chat goes here
-				</Typography>
+			<Container maxWidth="xl" component="main" className={classes.mainContent}>
+				<Paper elevation={3} className={classes.chatsRoot}>
+					{ chatsPanel }
+					<div className={classes.flexGrow}>
+						<Typography>hi</Typography>
+					</div>
+					{ chatsPanel }
+				</Paper>
 			</Container>
 		</Page>
 	);
