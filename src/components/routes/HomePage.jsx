bbcceaacac85b4d522d2651a6e83497d15d06881
zainja <zainjaffal@gmail.com>
2020-09-02 19:02:34 +0100

    removing event card

--- src/components/routes/HomePage.jsx
+++ src/components/routes/HomePage.jsx
@@ -23,9 +23,6 @@ export default function Homepage() {
 				<Typography variant="h5" align="center" color="textSecondary" component="p">
           We're helping to connect University of Manchester students with each other online in light of the COVID-19 pandemic!
 				</Typography>
-				<EventCard title="Event" description="dsdsd" startDate="23/12" endDate="12/12"/>
-				<LoadingEventCard/>
-				<EventCard title="Event" description="dsds" startDate="22/12" endDate="12/12" expired={true}/>
 			</Container>
 			{/* End hero unit */}
 			<Footer />
