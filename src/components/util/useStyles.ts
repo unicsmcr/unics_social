c52949e2dc6083e666002ab07a1a0a8b9ac2327c
zainja <zainjaffal@gmail.com>
2020-09-02 19:35:44 +0100

    lint fix

--- src/components/util/useStyles.ts
+++ src/components/util/useStyles.ts
@@ -1,48 +1,48 @@
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: 'none'
-        }
-    },
-    'appBar': {
-        borderBottom: `1px solid ${theme.palette.divider}`
-    },
-    'toolbar': {
-        flexWrap: 'wrap'
-    },
-    'toolbarTitle': {
-        flexGrow: 1
-    },
-    'link': {
-        margin: theme.spacing(1, 1.5)
-    },
-    'heroContent': {
-        padding: theme.spacing(28, 2, 28, 2)
-    },
+	'@global': {
+		ul: {
+			margin: 0,
+			padding: 0,
+			listStyle: 'none'
+		}
+	},
+	'appBar': {
+		borderBottom: `1px solid ${theme.palette.divider}`
+	},
+	'toolbar': {
+		flexWrap: 'wrap'
+	},
+	'toolbarTitle': {
+		flexGrow: 1
+	},
+	'link': {
+		margin: theme.spacing(1, 1.5)
+	},
+	'heroContent': {
+		padding: theme.spacing(28, 2, 28, 2)
+	},
 
-    'paddedCaption': {
-        padding: theme.spacing(2, 0, 2, 0)
-    },
-    'eventCard': {
-        width: 345
-    },
-    'footer': {
-        borderTop: `1px solid ${theme.palette.divider}`,
-        marginTop: theme.spacing(8),
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(3),
-        [theme.breakpoints.up('sm')]: {
-            paddingTop: theme.spacing(6),
-            paddingBottom: theme.spacing(6)
-        }
-    },
-    'media': {
-        height: 140
-    }
+	'paddedCaption': {
+		padding: theme.spacing(2, 0, 2, 0)
+	},
+	'eventCard': {
+		width: 345
+	},
+	'footer': {
+		borderTop: `1px solid ${theme.palette.divider}`,
+		marginTop: theme.spacing(8),
+		paddingTop: theme.spacing(3),
+		paddingBottom: theme.spacing(3),
+		[theme.breakpoints.up('sm')]: {
+			paddingTop: theme.spacing(6),
+			paddingBottom: theme.spacing(6)
+		}
+	},
+	'media': {
+		height: 140
+	}
 }));
 
 export default useStyles;
