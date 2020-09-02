bebb1f09c242440b2ac11c6b8f8dbc9792424576
zainja <zainjaffal@gmail.com>
2020-09-02 20:38:00 +0100

    improving imports

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import { CardContent, Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography'
+import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import useStyles from './util/useStyles';
