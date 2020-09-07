67ae51ac69eb803a6d59008720c08d322aa3f243
Amish Shah <amishshah.2k@gmail.com>
2020-09-07 18:49:18 +0100

    refactor: use backdrop while connecting to gateway

--- src/components/util/ProtectedRoute.tsx
+++ src/components/util/ProtectedRoute.tsx
@@ -4,18 +4,36 @@ import { Route, Redirect, withRouter } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectJWT, selectConnected } from '../../store/slices/AuthSlice';
 import { initClientGateway, client } from './makeClient';
+import { makeStyles, Backdrop, CircularProgress, Typography, Box } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	backdrop: {
+		'zIndex': theme.zIndex.drawer + 1,
+		'color': '#fff',
+		'flexDirection': 'column',
+		'& > *': {
+			margin: theme.spacing(4, 0)
+		}
+	}
+}));
 
 const ProtectedRoute = props => {
 	const { component: Component, ...rest } = props;
 	const jwt = useSelector(selectJWT);
 	const connected = useSelector(selectConnected);
+	const classes = useStyles();
 
 	let fallback: ReactNode|undefined;
 	if (!jwt) {
 		fallback = <Redirect to="/login" />;
 	} else if (!connected) {
 		initClientGateway(client);
-		fallback = <h2>Connecting to gateway...</h2>;
+		fallback = <Backdrop open={true} className={classes.backdrop}>
+			<Typography variant="h4">Connecting...</Typography>
+			<Box>
+				<CircularProgress color="inherit" />
+			</Box>
+		</Backdrop>;
 	}
 
 	return (
