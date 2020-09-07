2b41475ee152df2199629c09428348f48be72e61
HishamYahya <38282695+HishamYahya@users.noreply.github.com>
2020-09-07 19:34:02 +0300

    style: fix linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import AccountSettingsPage from './components/routes/AccountSettings';
 import ProtectedRoute from './components/util/ProtectedRoute';
 import PublicRoute from './components/util/PublicRoute';
 import { Provider } from 'react-redux';
-import store from './store';
 import ChatPage from './components/routes/chat/ChatsPage';
 
 function App() {
