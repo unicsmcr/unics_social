b857d4ea2bae4d48f745e6823037a61d4c1cf986
Zain Alden Jaffal <54101281+zainja@users.noreply.github.com>
2020-09-08 17:37:34 +0100

    moving event file

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import './event.css';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
