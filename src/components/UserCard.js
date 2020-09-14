2623a17ccfb2007607a50c0a1033605c133c0944
Zain Alden Jaffal <54101281+zainja@users.noreply.github.com>
2020-09-14 16:19:08 +0100

    feat: creating a user card

---
+++ src/components/UserCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Paper} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const UserCard = ({username, fullName, course, year}) => {
+
+    return (
+        <Paper>
+
+        </Paper>
+    )
+}
+export default UserCard
