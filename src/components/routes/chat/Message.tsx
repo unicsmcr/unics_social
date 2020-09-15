fc95dd5863619737d4ba5ef88822eb5322289975
Amish Shah <amishshah.2k@gmail.com>
2020-09-15 15:09:42 +0100

    refactor: improve message bubble times

--- src/components/routes/chat/Message.tsx
+++ src/components/routes/chat/Message.tsx
@@ -3,36 +3,43 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import grey from '@material-ui/core/colors/grey';
 import { makeStyles } from '@material-ui/core/styles';
+import moment from 'moment';
+import { OptimisedAPIMessage } from '../../../store/slices/MessagesSlice';
 
 export interface MessageProps {
-	content: string;
+	message: OptimisedAPIMessage;
 	id: string;
 	isOwn: boolean;
 }
 
+function formatTime(time: number): string {
+	return moment(time).format('h:mma');
+}
+
 const useStyles = makeStyles(theme => ({
-	messageBubble: {
+	messageBubble: (props: any) => ({
 		display: 'inline-block',
-		padding: theme.spacing(2),
-		background: grey[100],
-		borderRadius: theme.spacing(0, 4, 4, 4),
+		padding: theme.spacing(1.5, 2, 1.5, 2),
+		background: props.isOwn ? theme.palette.primary.light : grey[100],
+		borderRadius: props.isOwn ? theme.spacing(4, 4, 0, 4) : theme.spacing(0, 4, 4, 4),
 		marginBottom: theme.spacing(1),
-		maxWidth: '70ch'
-	},
-	ownMessage: {
-		background: theme.palette.primary.light,
-		color: theme.palette.primary.contrastText,
-		borderRadius: theme.spacing(4, 4, 0, 4)
-	}
+		maxWidth: '70ch',
+		color: props.isOwn && theme.palette.primary.contrastText
+	}),
+	time: (props: any) => ({
+		color: props.isOwn ? 'rgba(255,255,255,0.5)' : 'rgba(0,0,0,0.5)',
+		marginLeft: theme.spacing(1)
+	})
 }));
 
-export default function Message({ content, isOwn }: MessageProps) {
-	const classes = useStyles();
+export default function Message({ message, isOwn }: MessageProps) {
+	const classes = useStyles({ isOwn });
 
 	return <Box>
-		<div className={[classes.messageBubble, isOwn ? classes.ownMessage : ''].join(' ')}>
+		<div className={classes.messageBubble}>
 			<Typography variant="body1" style={{ textAlign: 'left' }}>
-				{ content }
+				{ message.content }
+				<Typography variant="caption" className={classes.time}>{ formatTime(message.time) }</Typography>
 			</Typography>
 		</div>
 	</Box>;
