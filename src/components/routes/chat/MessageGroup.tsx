4ef33ad1cbfaae8e7d08b7e2780d5f7551fbfb2f
Amish Shah <amishshah.2k@gmail.com>
2020-09-15 13:35:56 +0100

    feat: add timestamps to groups

--- src/components/routes/chat/MessageGroup.tsx
+++ src/components/routes/chat/MessageGroup.tsx
@@ -11,6 +11,7 @@ import { fetchUser, selectUserById } from '../../../store/slices/UsersSlice';
 import getIcon from '../../util/getAvatar';
 import { Skeleton } from '@material-ui/lab';
 import clsx from 'clsx';
+import moment from 'moment';
 
 export enum Align {
 	Left = 'left',
@@ -23,12 +24,13 @@ interface MessageGroupProps {
 	align: Align;
 }
 
+function formatTime(time: Date): string {
+	return moment(time).format('h:mm a');
+}
+
 const useStyles = makeStyles(theme => ({
 	messageGroup: {
-		display: 'block',
-		padding: theme.spacing(2),
-		background: grey[200],
-		borderRadius: theme.spacing(4)
+		paddingBottom: theme.spacing(1)
 	},
 	avatar: {
 		marginRight: theme.spacing(1),
@@ -49,6 +51,9 @@ const useStyles = makeStyles(theme => ({
 		'& > *:last-child': {
 			gridColumn: '1 / -1'
 		}
+	},
+	rightAlign: {
+		justifyContent: 'flex-end'
 	}
 }));
 
@@ -60,7 +65,8 @@ export function createGroups(messages: OptimisedAPIMessage[], relativeTo: string
 			currentGroup.push(message);
 			continue;
 		}
-		if (message.authorID === currentGroup[0].authorID) {
+		const lastMessage = currentGroup[currentGroup.length - 1];
+		if (message.authorID === lastMessage.authorID && message.time - lastMessage.time <= 3 * 60 * 1000) {
 			currentGroup.push(message);
 		} else {
 			groups.push(currentGroup);
@@ -95,13 +101,19 @@ export default function MessageGroup({ messages, align, authorID }: MessageGroup
 		</Box>;
 	}
 
-	return <Box style={{ textAlign: align }}>
-		{ align === Align.Left && <Box className={classes.userInfo} >
-			<Avatar src={getIcon(author)} className={classes.avatar} />
-			<Typography variant="subtitle2">
-				{author.forename}
-			</Typography>
-		</Box>
+	return <Box style={{ textAlign: align }} className={classes.messageGroup}>
+		{ align === Align.Left
+			? <Box className={classes.userInfo} >
+				<Avatar src={getIcon(author)} className={classes.avatar} />
+				<Typography variant="subtitle2">
+					{author.forename} | {formatTime(new Date(messages[0].time))}
+				</Typography>
+			</Box>
+			: <Box className={clsx(classes.userInfo, classes.rightAlign)} >
+				<Typography variant="subtitle2" align="right">
+					{formatTime(new Date(messages[0].time))}
+				</Typography>
+			</Box>
 		}
 		{
 			messages.map(message => <Message content={message.content} id={message.id} isOwn={align === Align.Right} key={message.id} />)
