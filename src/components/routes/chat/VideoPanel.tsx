674218288a3fdef027f85b38f10bb0f12df95aa5
Amish Shah <amishshah.2k@gmail.com>
2020-09-16 10:00:26 +0100

    feat: cleanup

--- src/components/routes/chat/VideoPanel.tsx
+++ src/components/routes/chat/VideoPanel.tsx
@@ -1,5 +1,5 @@
 import { APIDMChannel } from '@unicsmcr/unics_social_api_client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Video from 'twilio-video';
 
 interface VideoPanelProps {
@@ -11,31 +11,44 @@ export default function VideoPanel(props: VideoPanelProps) {
 	const [room, setRoom] = React.useState<Video.Room|undefined>();
 	const [mediaStream, setMediaStream] = React.useState<MediaStream|undefined>();
 
-	const ourVideoRef = React.createRef<HTMLVideoElement>();
+	const ourVideoRef = useRef<HTMLVideoElement|null>(null);
 
 	useEffect(() => {
+		let _room: Video.Room;
+		let _mediaStream: MediaStream;
 		async function launch() {
 			if (!room) {
-				const mediaStream = await navigator.mediaDevices.getUserMedia({
+				_mediaStream = await navigator.mediaDevices.getUserMedia({
 					audio: true,
 					video: true
 				});
-				setMediaStream(mediaStream);
-				console.log('got', mediaStream);
-				const videoEl = document.querySelector('video')!;
-				const videoTrack = mediaStream.getVideoTracks()[0];
+				setMediaStream(_mediaStream);
+				console.log('got', _mediaStream);
+				const videoEl = ourVideoRef.current;
+				if (!videoEl) return;
+				const videoTrack = _mediaStream.getVideoTracks()[0];
 
 				videoEl.width = videoTrack.getSettings().width!;
 				videoEl.height = videoTrack.getSettings().height!;
 
-				videoEl.srcObject = mediaStream;
+				videoEl.srcObject = _mediaStream;
 
-				const room = await Video.connect(props.videoJWT, { name: props.channel.id, tracks: mediaStream.getTracks() });
-				setRoom(room);
-				console.log(room);
+				_room = await Video.connect(props.videoJWT, { name: props.channel.id, tracks: _mediaStream.getTracks() });
+				setRoom(_room);
+				console.log(_room);
 			}
 		}
+
 		launch();
+
+		return () => {
+			if (_mediaStream) {
+				_mediaStream.getTracks().forEach(track => track.stop());
+			}
+			if (_room) {
+				_room.disconnect();
+			}
+		};
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
