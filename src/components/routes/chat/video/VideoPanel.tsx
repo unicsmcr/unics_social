1d2d0c85a70debb48cc5ca27929105251a61d3ae
Amish Shah <amishshah.2k@gmail.com>
2020-09-16 18:12:59 +0100

    feat: start to work on error reporting

--- src/components/routes/chat/video/VideoPanel.tsx
+++ src/components/routes/chat/video/VideoPanel.tsx
@@ -2,7 +2,9 @@ import Box from '@material-ui/core/Box';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { APIDMChannel } from '@unicsmcr/unics_social_api_client';
 import React, { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Video, { createLocalTracks } from 'twilio-video';
+import NotificationDialog from '../../../util/NotificationDialog';
 import OptionsPanel from './OptionsPanel';
 import PeerVideo from './PeerVideo';
 import SelfVideo from './SelfVideo';
@@ -27,8 +29,16 @@ const useStyles = makeStyles(theme => ({
 	}
 }));
 
+function wrapError<T>(promise: Promise<T>, message: string): Promise<T> {
+	return promise.catch(error => {
+		console.error(error);
+		return Promise.reject(new Error(message));
+	});
+}
+
 export default function VideoPanel(props: VideoPanelProps) {
 	const classes = useStyles();
+	const history = useHistory();
 
 	const selfVideoRef = useRef<HTMLVideoElement>(null);
 	const peerVideoRef = useRef<HTMLVideoElement>(null);
@@ -38,24 +48,26 @@ export default function VideoPanel(props: VideoPanelProps) {
 	const [cameraMode, setCameraMode] = useState<'user'|'environment'>('user');
 	const [tracks, setTracks] = useState<Video.LocalTrack[]>();
 
+	const [error, setError] = useState<string>();
+
 	useEffect(() => {
 		let _room: Video.Room;
 		let _tracks: Video.LocalTrack[];
 
 		async function launch() {
 			if (!room) {
-				const tracks = await createLocalTracks({
+				const tracks = await wrapError(createLocalTracks({
 					audio: true,
 					video: {
 						facingMode: cameraMode
 					}
-				});
+				}), 'Failed to find a camera and/or microphone');
 				_tracks = tracks;
 				setTracks(tracks);
 
 				if (selfVideoRef.current) {
 					const videoTrack: Video.LocalVideoTrack|undefined = tracks.find(track => track.kind === 'video') as Video.LocalVideoTrack;
-					if (!videoTrack) throw new Error('No video');
+					if (!videoTrack) throw new Error('Camera not found');
 					videoTrack.attach(selfVideoRef.current);
 				}
 
@@ -87,7 +99,11 @@ export default function VideoPanel(props: VideoPanelProps) {
 			}
 		}
 
-		launch();
+		launch()
+			.catch(error => {
+				console.error('Error in video:', error);
+				setError(error.message);
+			});
 
 		return () => {
 			if (_tracks) {
@@ -141,6 +157,12 @@ export default function VideoPanel(props: VideoPanelProps) {
 					}
 				}
 			}}
+		/>
+		<NotificationDialog
+			title="Error in video chat"
+			show={Boolean(error?.length)}
+			message={error ?? ''}
+			onClose={() => history.push(`${history.location.pathname.replace('/video', '')}`)}
 		/>
 	</Box>;
 }
