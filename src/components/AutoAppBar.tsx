fdc21c46421243a3655f327e16ed611981846077
Amish Shah <amishshah.2k@gmail.com>
2020-09-21 21:36:02 +0100

    refactor: move chats first

--- src/components/AutoAppBar.tsx
+++ src/components/AutoAppBar.tsx
@@ -130,8 +130,8 @@ export default function AutoAppBar() {
 						{
 							hasJWT
 								? <>
-									<Button color="inherit" component={RouterLink} to="/networking">1:1 Networking</Button>
 									<Button color="inherit" component={RouterLink} to="/chats">Chats</Button>
+									<Button color="inherit" component={RouterLink} to="/networking">1:1 Networking</Button>
 									<Button color="inherit" component={RouterLink} to="/account">Account</Button>
 									<Button href="#" color="inherit" variant="outlined" className={classes.link} onClick={logout}>
 											Logout
