386c93008926105a13b1d37710ab4a6bc2a94edd
Amish Shah <amishshah.2k@gmail.com>
2020-09-25 14:00:50 +0100

    feat: start to redo homepage

--- src/components/routes/HomePage.tsx
+++ src/components/routes/HomePage.tsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import useStyles from '../util/useStyles';
 import Page from '../Page';
+import { Box, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	'@global': {
+		ul: {
+			margin: 0,
+			padding: 0,
+			listStyle: 'none'
+		}
+	},
+	'heroContent': {
+		padding: theme.spacing(16, 2, 16, 2),
+		backgroundColor: '#520F79',
+		background: `url(${require('../../assets/homepage_bg.jpg')})`,
+		backgroundSize: 'cover',
+		minHeight: '70vh',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	'heroText': {
+		background: 'black',
+		color: 'white',
+		display: 'inline-block',
+		fontSize: '3rem',
+		fontWeight: 'bold',
+		padding: theme.spacing(2)
+	}
+}));
+
 export default function Homepage() {
 	const classes = useStyles();
-
 	return (
 		<Page>
-			{/* Hero unit */}
-			<Container maxWidth="sm" component="main" className={classes.heroContent}>
-				<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-					UniCS KB
-				</Typography>
-				<Typography variant="h5" align="center" color="textSecondary" component="p">
-					We're helping to connect University of Manchester students with each other online in light of the COVID-19 pandemic!
-				</Typography>
-			</Container>
-			{/* End hero unit */}
+			<Box className={classes.heroContent} component="header">
+				{/* Hero unit */}
+				<Container maxWidth="sm">
+					<Typography variant="h1" align="center" color="textPrimary" gutterBottom className={classes.heroText}>
+						Welcome to UniCS <span style={{ color: 'gold' }}>KB</span>
+					</Typography>
+				</Container>
+				{/* End hero unit */}
+			</Box>
 		</Page>
 	);
 }
