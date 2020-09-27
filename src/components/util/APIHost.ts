b2bf00bff4afd7d12cee5c4bde87b31e4b27ff60
Amish Shah <amishshah.2k@gmail.com>
2020-09-27 16:41:20 +0100

    feat: match profile URLs

--- src/components/util/APIHost.ts
+++ src/components/util/APIHost.ts
@@ -1,3 +1,3 @@
-const API_HOST = process.env.NODE_ENV === 'production' ? 'https://kb-api.unicsmcr.com' : 'http://localhost:8060';
+const API_HOST = process.env.NODE_ENV === 'production' ? 'https://kb-api.unicsmcr.com' : 'http://192.168.0.30:8060';
 
 export default API_HOST;
