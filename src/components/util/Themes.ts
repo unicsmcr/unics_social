03cf1763fbc4f657e5af1b10487fe7b16d58b4b6
Amish Shah <amishshah.2k@gmail.com>
2020-10-27 11:31:14 +0000

    Update src/components/util/Themes.ts

--- src/components/util/Themes.ts
+++ src/components/util/Themes.ts
@@ -1,7 +1,7 @@
 export function isHalloween() {
 	const dateToday = new Date();
 
-	if ((dateToday.getMonth() === 9 && dateToday.getDate() >= 30) || (dateToday.getMonth() === 10 && dateToday.getDate() === 1)) {
+	if ((dateToday.getMonth() === 9 && dateToday.getDate() >= 29) || (dateToday.getMonth() === 10 && dateToday.getDate() === 1)) {
 		return true;
 	}
 
