cb4e80fcf88cae37d64921c30579e83c47ce5874
Reean Khan <reean.khan@student.manchester.ac.uk>
2020-11-02 16:06:13 +0000

    amish inspired fix

--- src/components/routes/chat/MessagesPanel.tsx
+++ src/components/routes/chat/MessagesPanel.tsx
@@ -90,7 +90,7 @@ export default function MessagesPanel(props: MessagesPanelProps) {
 	}, [props.channel.id]);
 
 	useEffect(() => {
-		if (scrollSynced) {
+		if (scrollSynced && document.hasFocus()) {
 			dispatch(readChannel({ channelID: props.channel.id, time: Date.now() }));
 		}
 	}, [props.channel.id, scrollSynced, messages.length, dispatch]);
