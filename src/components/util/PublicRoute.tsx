0c285a25d326242e477fdb27fc7c745013180173
Regson Do Rego <32569720+RegsonDR@users.noreply.github.com>
2020-12-09 15:40:04 +0000

    refactor: default to /chats after successful login (#141)

--- src/components/util/PublicRoute.tsx
+++ src/components/util/PublicRoute.tsx
@@ -7,12 +7,16 @@ import { selectJWT } from '../../store/slices/AuthSlice';
 const PublicRoute = props => {
 	const { component: Component, ...rest } = props;
 	const jwt = useSelector(selectJWT);
+
+	// Note: user will be redirected to /account if their profile is not set
+	const userLandingPage = '/chats';
+
 	return (
 		<Route
 			{...rest}
 			render={props =>
 				jwt
-					? <Redirect to="/account" />
+					? <Redirect to={userLandingPage} />
 					: <Component {...props} />
 			}
 		/>
